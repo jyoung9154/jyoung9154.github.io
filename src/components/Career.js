fb0d2c174a25a5848b5dc34257c26ccc54188180
JaeYoung Park <jyoung_9154@naver.com>
2025-03-05 00:25:33 +0900

    Update Career.js

--- src/components/Career.js
+++ src/components/Career.js
@@ -7,15 +7,20 @@ export default function Career() {
                 <h2 className="career-header">📜 경력기술서</h2>
                 <pre className="career-text" style={{ backgroundColor: 'inherit', fontSize: '14px', border: 'none' }}>
                 <h3>🏢 더존비즈온</h3>
-                솔루션사업본부 UC개발팀 [2018.09 ~ 2022.06 (3년 10개월)] <br/>
-                - 1977년 설립, 매출액 3,500억, 사원 수 : 1764명<br/>
+                솔루션사업본부 UC개발팀 메일 Unit [2024.06 ~ ]                
+                솔루션사업본부 UC개발팀 전자결재 Unit [2018.09 ~ 2022.06 (3년 10개월)] <br/>
+                
+                - 1977년 설립, 소프트웨어 개발업, 매출액 4,000억, 사원 수 : 1792명<br/>
                 <br/>
                 <h3>🖊️ 주요 업무</h3>
                 <b style={{ fontSize: '16px' }}>1.그룹웨어 전자결재 백엔드, 프론트엔드 개발 및 유지보수</b><br/>
-                - 기술 스택 : Java(Spring), Spring Boot, JSP, React, MySQL, Redis, Git, SVN, Docker<br/>
-                <b style={{ fontSize: '16px' }}>2.그룹웨어 전자결재 마이그레이션 개발 및 진행</b><br/>
+                - 기술 스택 : Java(Spring), Spring Boot, JSP, React, MySQL, Redis, Git, SVN, Docker, k8s <br/>
+                <b style={{ fontSize: '16px' }}>2.그룹웨어 전자결재 마이그레이션 개발 및 운영 담당</b><br/>
                 - 기술 스택: Linux, Spring Boot(JPA), React, MySQL, MSSQL, Oracle DB, Git<br/>
+                <b style={{ fontSize: '16px' }}>3.그룹웨어 메일 개발 및 유지보수</b><br/>
+                - 기술 스택: Linux, Spring Boot, React, MySQL, Git, k8s, MailEngine(SMTP, POP, IMAP), ElasticSearch, Kafka <br/>
                 <br/>
+                    
                 <h3>🧑‍💻 진행 프로젝트</h3>
                 <b style={{ fontSize: '16px' }}>1. 전자결재 데이터 마이그레이션</b><br/>
                 (1) 개발 기간 : 2018.12 ~ 2022.06 (3년 7개월)<br/>
@@ -27,6 +32,7 @@ export default function Career() {
                 (5) 성과 : 수동으로 진행하였던 마이그레이션을 툴 개발로 인해 마이그레이션 소요시간 대폭 단축 (소요시간 3일 ➡️ 2시간) 및 고객사 업세일 진행속도 증가에 따른 그룹웨어 매출액 증가<br/>
                 - 약 500여 고객사 마이그레이션 진행<br/>
                 </pre>
+                
                 <pre className="career-text" style={{ backgroundColor: 'inherit', fontSize: '14px', border: 'none' }}>
                 <b style={{ fontSize: '16px' }}>2. Amaranth10(그룹웨어) 전자결재 개발 및 유지보수 </b><br/>
                 (1) 개발 기간 : 2019.11 ~ 2022.06 (2년 7개월)<br/>
@@ -43,13 +49,23 @@ export default function Career() {
                 (4) 개발 및 유지보수 기여도 : 35%<br/>
                 (5) 성과 : 신규 프로그램 개발로 사용자 경험 확대 및 다양한 옵션과 기능 개선 및 추가로 고객 편의 제공 및 제품 판매 이후 200여건 유지보수 진행 <br/>
                 </pre>
+                
                 <pre className="career-text" style={{ backgroundColor: 'inherit', fontSize: '14px', border: 'none' }}>
                 <b style={{ fontSize: '16px' }}>3. BizboxAlpha(그룹웨어) 전자결재 개발 및 유지보수</b><br/>
                 (1) 개발 기간 : 2018.09 ~ 2021.03 (2년 6개월)<br/>
                 (2) 개발 인원 : 4명<br/>
                 (3) 주요 개발 내역 :  전자결재 내 주요 기능 고도화 및 약 800여건 유지보수 진행<br/>
-                (4) 개발 및 유지보수 기여도 : 20%<br/>
+                (4) 개발 및 유지보수 기여도 : 30%<br/>
                 (5) 성과 : 기능 고도화 및 유지보수를 진행함으로써 사용자 편의 제공<br/>
+                </pre>
+                
+                <pre className="career-text" style={{ backgroundColor: 'inherit', fontSize: '14px', border: 'none' }}>
+                <b style={{ fontSize: '16px' }}>4. 그룹웨어 메일 개발 및 유지보수</b><br/>
+                (1) 개발 기간 : 2024.06 ~ <br/>
+                (2) 개발 인원 : 4명<br/>
+                (3) 주요 개발 내역 : 메일 통계, 메일 탈취 감지, 메일 릴레이설정 사용자화, 스팸장비 연동 개발<br/>
+                (4) 개발 및 유지보수 기여도 : 100%<br/>
+                (5) 성과 : 신규기능 개발로 사용자 편의 제공 <br/>
                 </pre>
                 
             </article>
